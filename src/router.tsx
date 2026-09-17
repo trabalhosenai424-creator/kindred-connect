b3238131827187806cb4d3501537ee9416560f61
trabalhosenai424-creator <trabalhosenai424@gmail.com>
2026-09-17 07:46:28 -0300

    feat: ativar central de notificacoes no app

--- src/router.tsx
+++ src/router.tsx
@@ -2,9 +2,11 @@ import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import { ensurePushSubscription } from "./lib/push-notifications";
+import { installNotificationBell } from "./lib/notification-widget";
 
 if (typeof window !== "undefined") {
   void ensurePushSubscription();
+  installNotificationBell();
 }
 
 export const getRouter = () => {
